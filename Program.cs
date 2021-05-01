cbec3bdb5caf8fcebd6ce887ab8a449223e9d11b
Carlos Ferreira <>
2021-05-01 09:09:05 -0400

    dev-mac

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KB9000Utility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
